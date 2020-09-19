8afb117bd2d27518e525a6a2502a96410725a74e
alyssabartuch <alyssabartuch@gmail.com>
2020-09-18 20:02:31 -0500

    spotify auth config, architectural setup, spotify-web-api-js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Login from "./Login";
 import "./App.css";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>spotify clone</h1>
-      </header>
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+    }
+  }, []);
+
+  return <div className="App">{token ? <h1>LOGGED IN</h1> : <Login />}</div>;
 }
 
 export default App;

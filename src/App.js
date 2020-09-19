c4919460c5d64291c9a9c59318e4932d155a8163
alyssabartuch <alyssabartuch@gmail.com>
2020-09-18 23:04:19 -0500

    sidebar styling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React, { useState, useEffect } from "react";
-import Login from "./Login";
-import Player from "./Player";
 import "./App.css";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Login from "./Login";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
       spotify.setAccessToken(_token);
       spotify.getMe().then((user) => {
-        console.log(user);
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
   }, []);
-
-  return <div className="App">{token ? <Player /> : <Login />}</div>;
+  console.log("USER", user);
+  console.log("TOKEN", token);
+  return (
+    <div className="App">
+      {token ? <Player spotify={spotify} /> : <Login />}
+    </div>
+  );
 }
 
 export default App;
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,8 @@ export const initialState = {
   playlists: [],
   isPlaying: false,
   item: null,
+  token:
+    "BQDRXaceX6xuuKTj93KfFEQspwDVPKOAIw4HkooR4ayKelfJr_AWvMUgiXzLsFfC-ws5996SuIpJIylzIVpwXoAHchxerH1AKFqn0qndMlLQ5XXTe5ZaCDCyJYyma0CHPb6fXnk2nk-aiq2QQmldI4ZH0DEePRL3",
 };
 
 const reducer = (state, action) => {
@@ -13,6 +15,11 @@ const reducer = (state, action) => {
       return {
         ...state,
         user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
       };
     default:
       return state;
